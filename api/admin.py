bda8d7a15892cd811493f4bddfb89b8d8f754b74
JHeaon <j3heawon@gmail.com>
2024-07-11 18:29:55 +0900

    Feat: API앱 모델들 관리자페이지 관리 기능 추가

--- api/admin.py
+++ api/admin.py
@@ -1,1 +1,43 @@
 from django.contrib import admin
+from api.models import (
+    CommunityPost,
+    CommunityComment,
+    NewsPost,
+    NewsComment,
+    SupportPost,
+)
+
+
+@admin.register(CommunityPost)
+class CommunityPostAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "create_at")
+    search_fields = ("title", "contents")
+    list_filter = ("create_at", "user")
+
+
+@admin.register(CommunityComment)
+class CommunityCommentAdmin(admin.ModelAdmin):
+    list_display = ("community_post", "user", "create_at")
+    search_fields = ("contents", "community_post__title")
+    list_filter = ("create_at", "user")
+
+
+@admin.register(NewsPost)
+class NewsPostAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "create_at")
+    search_fields = ("title", "contents")
+    list_filter = ("create_at", "user")
+
+
+@admin.register(NewsComment)
+class NewsCommentAdmin(admin.ModelAdmin):
+    list_display = ("news_post", "user", "create_at")
+    search_fields = ("contents", "news_post__title")
+    list_filter = ("create_at", "user")
+
+
+@admin.register(SupportPost)
+class SupportPostAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "create_at")
+    search_fields = ("name", "email", "contents")
+    list_filter = ("create_at",)
